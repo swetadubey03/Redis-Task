6b3bde1f69d875b7b592a24c5ebc37b3e585368d
Sweta <sweta.dubey@wohlig.com>
2022-10-10 15:01:42 +0530

    Redis Final version

--- controller/weatherController.js
+++ controller/weatherController.js
@@ -1,16 +1,15 @@
-const axios = require('axios');
+const axios = require('axios')
 const redis = require('redis')
-
-//for expiration date
-const EXPIRATION = 20
 
 const redisClient = redis.createClient()
 
+//for expiration date
+const EXPIRATION = 60
 
 
 const redisAPI = async(req,res)=>{
     redisClient.get("weatherdata", async(error, weatherdata)=>{
-        
+
         if(error){
             console.log(error)
         }
@@ -18,12 +17,14 @@ const redisAPI = async(req,res)=>{
         if (weatherdata != null){
             return res.json(JSON.parse(weatherdata))
         }else{
-            const {weatherdata} = await axios.get(
-                `https://api.openweathermap.org/data/2.5/weather?q=London&appid=63bdb51dde03c0c24aff2141d65d6fe5`
+            const {data} = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=63bdb51dde03c0c24aff2141d65d6fe5`
 
             )
-            redisClient.setEx("weatherdata", EXPIRATION, JSON.stringify(weatherdata))
-            res.json(weatherdata)
+            redisClient.setex("weatherdata",EXPIRATION,JSON.stringify(data))
+               res.json(data)
+            
+            
         }
     })
 
